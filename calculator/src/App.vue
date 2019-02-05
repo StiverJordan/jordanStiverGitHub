cd2b8f657c752f4e1bc7bd321581929b114b01b0
Jordan Stiver <jordanstiver@Jordans-MacBook-Pro.local>
2019-02-05 13:45:54 -0500

    calculator

---
+++ calculator/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  background-color: #222;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 1rem;
+
+}
+</style>
